f8361004d30d739891b6c4e4fd4ae983db03e9d6
Ladroniel <you@example.com>
2025-11-21 18:44:16 +0200

    lab1_1

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,8 @@ std::string replaceNth(std::string text, int n, char old_value, char new_value){
         }
         return text;
 }
+
+
 int main(){
         std::string text = "Vader said: No, i am your father!";
 
